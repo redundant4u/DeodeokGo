26f548d7699de909857ea003b2543e32e7a926fa
redundant4u <physiogel@pusan.ac.kr>
2023-04-16 10:37:37 +0900

    fix: kafka 연결 수정

--- queue/kafka/connection.go
+++ queue/kafka/connection.go
@@ -7,11 +7,13 @@ import (
 
 func NewKafkaClient(cfg *viper.Viper) sarama.Client {
 	config := sarama.NewConfig()
-	brokers := []string{cfg.GetString("kafka.uri")}
+	brokers := []string{cfg.GetString("broker.kafka.uri")}
 	client, err := sarama.NewClient(brokers, config)
 	if err != nil {
 		panic("Colud not make kafka client: " + err.Error())
 	}
+
+	config.Producer.Return.Successes = true
 
 	return client
 }
